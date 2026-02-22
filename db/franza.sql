83e5da0b7ffeb48e99e0b040956bd9b1f92592fb
unknown <user@binit-l143.in.hq.binit.com.ar>
2026-02-22 18:21:58 -0300

    fix deleted obras

--- db/franza.sql
+++ db/franza.sql
@@ -1,39 +1,30 @@
-DROP TABLE IF EXISTS `obras`;
-DROP TABLE IF EXISTS `usuarios`;
-DROP TABLE IF EXISTS `roles`;
-
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 22-08-2025 a las 05:04:46
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: franza
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `franza`
+-- Table structure for table `obras`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `obras`
---
-
+DROP TABLE IF EXISTS `obras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `obras` (
-  `obra_id` int(10) UNSIGNED NOT NULL,
-  `usuario_fk` int(10) UNSIGNED NOT NULL,
+  `obra_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `usuario_fk` int(10) unsigned NOT NULL,
   `carpeta` varchar(100) NOT NULL,
   `titulo` varchar(100) NOT NULL,
   `ubicacion` varchar(100) NOT NULL,
@@ -41,128 +32,110 @@ CREATE TABLE `obras` (
   `tipo` varchar(100) NOT NULL,
   `imagen` varchar(255) NOT NULL,
   PRIMARY KEY (`obra_id`),
-  KEY `obras_usuarios_fk_idx` (`usuario_fk`)
+  KEY `obras_usuarios_fk_idx` (`usuario_fk`),
+  CONSTRAINT `obras_usuarios_fk` FOREIGN KEY (`usuario_fk`) REFERENCES `usuarios` (`usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `obras`
+--
+
+LOCK TABLES `obras` WRITE;
+/*!40000 ALTER TABLE `obras` DISABLE KEYS */;
+INSERT INTO `obras` VALUES (67,1,'reforma-de-azotea','Reforma de Azotea','San Miguel','La azotea presentaba problemas de filtraciones hacia la PB por fisuras en su revestimiento. Se reemplazaron los da├▒ados, previo a la reparaci├│n de la carpeta para generar el sello hidr├│fugo. Se quitaron y volvieron a generar las juntas de dilataci├│n con Poliuret├ínico de primera marca. Se pint├│ toda la superficie.','civil','AzoteaCaba_1771019121.jpg'),(68,1,'construccion-completa-de-hogar','Construccion completa de hogar','Barrio Cerrado ?','Explicaci├│n extensa de que se hizo','civil','CasaCharo_1771019219.jpg'),(69,1,'reforma-complete-de-balcon','Reforma complete de balcon','Villa del Parque','La zona de la terraza presentaba problemas de filtraciones hacia la PB por fisuras en su revestimiento. Se reemplazaron los da├▒ados, previo a la reparaci├│n de la carpeta para generar el sello hidr├│fugo. Se quitaron y volvieron a generar las juntas de dilataci├│n con Poliuret├ínico de primera marca. Se pint├│ toda la superficie. Paredes: Las mismas presentaban patolog├¡as graves por el ingreso constante de agua sobre un largo periodo de tiempo, generando asi una desconexi├│n entre el revoque y la mamp','civil','CasaVillaDelParque_1771019252.jpg'),(70,1,'obra-particular','Obra particular','Zona Norte GBA','Mediante un proyecto se realiz├│ la ampliaci├│n de una cocina existente en conexi├│n con un nuevo quincho. Se demolieron paredes y se generaron nuevos refuerzos. Mediante vigas de encadenados se mont├│ la nueva platea para soportar la carga del nuevo espacio. Se realiz├│ una parrilla completa, techo a dos aguas de tejas francesas, nuevas ventanas en techo de pa├▒o fijo, un ba├▒o y la ampliaci├│n de la cocina existente.','civil','JorgelinaZonaNorte_1771019309.jpg'),(71,1,'ampliaci-on-y-reforma-de-vivienda-unifamiliar','Ampliaci├│n y reforma de vivienda unifamiliar','San Isidro - GBA','Mediante un proyecto, se ejecut├│ esta obra que comprend├¡a el anexo de un nuevo espacio a uno existente y la reforma interna de los ambientes. Se realizaron tareas de hormig├│n visto - Pintura - Electricidad nueva - Gas - Aberturas - Techados - Aberturas Velux','civil','LambWeston_1771019353.jpg'),(72,1,'restauraci-on-de-fachada','Restauraci├│n de fachada','Mitre','Comenzamos con hidrolavado baja presi├│n, retiro de vegetaci├│n en griteas y sellado de las mismas. Se restauraron los ornamentos de la facha y se repararon las partes afectadas por la corrosi├│n debido a la exposici├│n del hierro de estructuras al agua. Se pint├│ con 4 manos de pintura para muros de primera calidad y se pintaron los frentes de los locales comerciales en la PB','civil','ObraBmeMitre_1771019414.jpg'),(73,1,'reforma-completa-de-hogar','Reforma completa de hogar','Tres de Febrero','La zona de la terraza presentaba problemas de filtraciones hacia la PB por fisuras en su revestimiento. Se reemplazaron los da├▒ados, previo a la reparaci├│n de la carpeta para generar el sello hidr├│fugo. Se quitaron y volvieron a generar las juntas de dilataci├│n con Poliuret├ínico de primera marca. Se pint├│ toda la superficie. Paredes: Las mismas presentaban patolog├¡as graves por el ingreso constante de agua sobre un largo periodo de tiempo, generando asi una desconexi├│n entre el revoque y la mamp','civil','ObraCanales_1771019471.jpg');
+/*!40000 ALTER TABLE `obras` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `obras_carrousel`
+--
+
+DROP TABLE IF EXISTS `obras_carrousel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `obras_carrousel` (
+  `obra_id` int(10) unsigned NOT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`obra_id`),
+  KEY `fk_obras_carrousel_obras1_idx` (`obra_id`),
+  CONSTRAINT `fk_obras_carrousel_obras1` FOREIGN KEY (`obra_id`) REFERENCES `obras` (`obra_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `obras`
+-- Dumping data for table `obras_carrousel`
 --
 
-INSERT INTO `obras` (`obra_id`, `usuario_fk`, `carpeta`, `titulo`, `ubicacion`, `explicacion`, `tipo`, `imagen`) VALUES
-(25, 1, 'CasaCharo', 'Construccion completa de hogar', 'Barrio Cerrado', 'Explicación extensa de que se hizo', 'civil', 'CasaCharo.jpg'),
-(26, 1, 'JorgelinaZonaNorte', 'Obra particular', 'Zona Norte GBA', 'Mediante un proyecto se realizó la ampliación de una cocina existente en conexión con un nuevo quincho. Se demolieron paredes y se generaron nuevos refuerzos. Mediante vigas de encadenados se montó la nueva platea para soportar la carga del nuevo espacio. Se realizó una parrilla completa, techo a dos aguas de tejas francesas, nuevas ventanas en techo de paño fijo, un baño y la ampliación de la cocina existente.', 'civil', 'JorgelinaZonaNorte.jpg'),
-(27, 1, 'AzoteaCaba', 'Reforma de Azotea', 'San Miguel', 'La azotea presentaba problemas de filtraciones hacia la PB por fisuras en su revestimiento. Se reemplazaron los dañados, previo a la reparación de la carpeta para generar el sello hidrófugo. Se quitaron y volvieron a generar las juntas de dilatación con Poliuretánico de primera marca. Se pintó toda la superficie.', 'civil', 'AzoteaCaba.jpg'),
-(28, 1, 'CasaVillaDelParque', 'Reforma complete de balcon', 'Villa del Parque', 'La zona de la terraza presentaba problemas de filtraciones hacia la PB por fisuras en su revestimiento. Se reemplazaron los dañados, previo a la reparación de la carpeta para generar el sello hidrófugo. Se quitaron y volvieron a generar las juntas de dilatación con Poliuretánico de primera marca. Se pintó toda la superficie. Paredes: Las mismas presentaban patologías graves por el ingreso constante de agua sobre un largo periodo de tiempo, generando asi una desconexión entre el revoque y la mamp', 'civil', 'CasaVillaDelParque.jpg'),
-(29, 1, 'ObraBmeMitre', 'Restauración de fachada', 'Mitre - CABA', 'Comenzamos con hidrolavado  baja presión, retiro de vegetación en griteas y sellado de las mismas. Se restauraron los ornamentos de la facha y se repararon las partes afectadas por la corrosión debido a la exposición del hierro de estructuras al agua. Se pintó con 4 manos de pintura para muros de primera calidad y se pintaron los frentes de los locales comerciales en la PB', 'civil', 'ObraBmeMitre.jpg'),
-(30, 1, 'LambWeston', 'Ampliación y reforma de vivienda unifamiliar', 'San Isidro - GBA', 'Mediante un proyecto, se ejecutó esta obra que comprendía el anexo de un nuevo espacio a uno existente y la reforma interna de los ambientes. Se realizaron tareas de hormigón visto - Pintura - Electricidad nueva - Gas - Aberturas - Techados - Aberturas Velux', 'civil', 'LambWeston.jpg'),
-(31, 1, 'ObraCanales', 'Reforma completa de hogar', '3 de Febrero', 'La zona de la terraza presentaba problemas de filtraciones hacia la PB por fisuras en su revestimiento. Se reemplazaron los dañados, previo a la reparación de la carpeta para generar el sello hidrófugo. Se quitaron y volvieron a generar las juntas de dilatación con Poliuretánico de primera marca. Se pintó toda la superficie. Paredes: Las mismas presentaban patologías graves por el ingreso constante de agua sobre un largo periodo de tiempo, generando asi una desconexión entre el revoque y la mamp', 'civil', 'ObraCanales.jpg'),
-(32, 1, 'test', 'Test de obra industrial', 'Ubicacion test obra indusltrial', 'Explicación extensa de que se hizo en la obra industrial', 'industrial', 'test.jpg'),
-(33, 1, 'CasaNueva', 'Construcción de casa nueva', 'Barrio Abierto', 'Descripción de la construcción de una casa nueva.', 'civil', 'CasaNueva.jpg');
-
--- --------------------------------------------------------
+LOCK TABLES `obras_carrousel` WRITE;
+/*!40000 ALTER TABLE `obras_carrousel` DISABLE KEYS */;
+/*!40000 ALTER TABLE `obras_carrousel` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `roles`
+-- Table structure for table `roles`
 --
 
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `roles` (
-  `rol_id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `rol_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`rol_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `roles`
+-- Dumping data for table `roles`
 --
 
-INSERT INTO `roles` (`rol_id`, `nombre`) VALUES
-(1, 'Administrador'),
-(2, 'Usuario');
-
--- --------------------------------------------------------
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Administrador'),(2,'Usuario');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Table structure for table `usuarios`
 --
 
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuarios` (
-  `usuario_id` int(10) UNSIGNED NOT NULL,
-  `rol_fk` int(10) UNSIGNED NOT NULL,
+  `usuario_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `rol_fk` int(10) unsigned NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`usuario_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `usuarios_roles_fk_idx` (`rol_fk`),
+  CONSTRAINT `usuarios_roles_fk` FOREIGN KEY (`rol_fk`) REFERENCES `roles` (`rol_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Dumping data for table `usuarios`
 --
 
-INSERT INTO `usuarios` (`usuario_id`, `rol_fk`, `email`, `password`) VALUES
-(1, 1, 'test@gmail.com', '$2y$10$M8GspSy2Td5nKvVqkXJ0d.qNlOYaeh3DYfjRncPaRtaqfl5TLY2MK'),
-(2, 2, 'asd@gmail.com', '$2y$10$SnP4uCkS76VNy8hqMZObA..x473qsKUJU/1KnJGEXOCNwRxrytNoa');
-
--- --------------------------------------------------------
-
---
--- Indices de la tabla `obras`
---
--- (elimina o comenta esta sección, ya no es necesaria)
--- ALTER TABLE `obras`
---   ADD PRIMARY KEY (`obra_id`),
---   ADD KEY `obras_usuarios_fk_idx` (`usuario_fk`);
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,1,'test@gmail.com','$2y$10$M8GspSy2Td5nKvVqkXJ0d.qNlOYaeh3DYfjRncPaRtaqfl5TLY2MK'),(2,2,'asd@gmail.com','$2y$10$SnP4uCkS76VNy8hqMZObA..x473qsKUJU/1KnJGEXOCNwRxrytNoa'),(3,2,'fernandezalejo981@gmail.com','$2y$10$ukLHlsAckU.IDPpQ4KvR1OIwri8OApMbqss5aluKu1ajlF6XVlqVO'),(6,1,'allegranza.emiliano@gmail.com','$2y$10$r8qLc24yeEmAmrVOOhGEEOhUFsD/CA4S4/QgXosnhwW5akJxNOaoq'),(7,2,'test2@gmail.com','$2y$10$plbED8nDBIvqNCs1PM2PHOfzp8/SPdt78/jNUBFnWRcvU22RR4.Gq');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Indices de la tabla `roles`
+-- Dumping routines for database 'franza'
 --
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`rol_id`);
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`usuario_id`),
-  ADD UNIQUE KEY `email_UNIQUE` (`email`),
-  ADD KEY `usuarios_roles_fk_idx` (`rol_fk`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `obras`
---
-ALTER TABLE `obras`
-  MODIFY `obra_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
-
---
--- AUTO_INCREMENT de la tabla `roles`
---
-ALTER TABLE `roles`
-  MODIFY `rol_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `usuario_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `obras`
---
-ALTER TABLE `obras`
-  ADD CONSTRAINT `obras_usuarios_fk` FOREIGN KEY (`usuario_fk`) REFERENCES `usuarios` (`usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_roles_fk` FOREIGN KEY (`rol_fk`) REFERENCES `roles` (`rol_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-13 22:06:35
